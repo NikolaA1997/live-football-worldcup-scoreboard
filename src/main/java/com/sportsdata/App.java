332298e40d13a7d3ff153e2348b7aea6654d203d
NikolaA1997 <nikolaandric.sem@gmail.com>
2025-03-14 18:52:16 +0100

    Initialize Maven project with basic structure and sample application

---
+++ src/main/java/com/sportsdata/App.java
@@ -0,0 +1,13 @@
+package com.sportsdata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
